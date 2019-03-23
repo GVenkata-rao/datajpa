8cbedbb75330539ff8efcf626e1dbe42690ab962
GVenkata-rao <venkatg526@gmail.com>
2019-03-23 21:40:16 +0530

    Resolved NullpointerException Issue

--- datajpa/src/main/java/com/datajpa/controller/ProductController.java
+++ datajpa/src/main/java/com/datajpa/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.datajpa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,14 +11,14 @@ import com.datajpa.service.ProductService;
 
 @RestController
 public class ProductController {
-
+	@Autowired
 	ProductService productService;
 
 	@RequestMapping(value = "saveProduct", method = RequestMethod.POST)
 	public Product saveProduct(@RequestBody Product product) {
 
-		return productService.save(product);
-		
+		 productService.save(product);
+
 	}
 
 }
